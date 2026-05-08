cded75cd2c6b42a02f336c188966eaa815bd9d7f
Irinajiroveanu <ijiroveanu@gmail.com>
2026-05-08 22:04:41 +0200

    feat: implement calendar page with sport filter and grouped daily view

--- app/Http/Controllers/CalendarController.php
+++ app/Http/Controllers/CalendarController.php
@@ -2,10 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Round;
+use App\Models\Sport;
+use Illuminate\Http\Request;
+
 class CalendarController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        return 'TODO: Calendar page';
+        $query = Round::with('sport', 'venue')->orderBy('date')->orderBy('start_time');
+
+        if ($request->filled('sport_id')) {
+            $query->where('sport_id', $request->sport_id);
+        }
+
+        $rounds = $query->get();
+
+        $roundsByDay = $rounds->groupBy(function ($round) {
+            return $round->date->format('Y-m-d');
+        });
+
+        $sports = Sport::orderBy('name')->get();
+
+        return view('calendar', [
+            'roundsByDay' => $roundsByDay,
+            'sports' => $sports,
+            'selectedSport' => $request->sport_id,
+        ]);
     }
 }
